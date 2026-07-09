1802dd7a6a0a3935df195e7e4bf0487757b576be
daxferab <daxferab.19@gmail.com>
2026-07-09 14:00:52 +0200

    Indent fixing

--- src/server/Channel.hpp
+++ src/server/Channel.hpp
@@ -7,23 +7,23 @@
 class Channel
 {
 	private:
-		const std::string		_name;
-		std::string				_topic;
-		std::string				_key;
+		const std::string	_name;
+		std::string			_topic;
+		std::string			_key;
 
-		std::set<int>			_users;
-		std::set<int>			_operators;
+		std::set<int>		_users;
+		std::set<int>		_operators;
 		
-		bool					_inviteonly;
-		bool					_topicRestrict;
-		int						_userLimit;
+		bool				_inviteonly;
+		bool				_topicRestrict;
+		int					_userLimit;
 
 	public:
 		Channel();
 		Channel(const std::string& name, int clientFd);
 		~Channel();
 
-		const std::string	&getName() const;
+		const std::string&	getName() const;
 
 };
 
