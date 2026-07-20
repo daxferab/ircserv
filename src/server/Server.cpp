1afbff58a2ffa28e3b69cbaf9d334abdd318d047
daxferab <daxferab.19@gmail.com>
2026-07-20 18:29:11 +0200

    fix kickuser

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -172,7 +172,7 @@ void	Server::kickUser(Client& client, const std::string& chanName, const std::st
 	Channel			*channel = _getChannel(chanName);
 
 	_fillContext(context, nick, chanName, "KICK", reason);
-	if (chanName.empty())
+	if (nick.empty() || chanName.empty())
 		_handleReply(client, AReply::getNReply(ERR_NEEDMOREPARAMS, *this, client, context));
 	else if(!channel)
 		_handleReply(client, AReply::getNReply(ERR_NOSUCHCHANNEL, *this, client, context));
